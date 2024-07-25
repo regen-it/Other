e26bfc2cf10bb79baa0670667188c20efdd2c92c
regen-it <34229816+regen-it@users.noreply.github.com>
2024-07-25 22:03:47 +1000

    Create PowerShellLogObjects.ps1

---
+++ Logging/PowerShellLogObjects.ps1
@@ -0,0 +1,34 @@
+#Template used for object based loggin in Active Directory based unattended scripts
+function Write-ActiveDirectoryLogLobject {
+<#
+.SYNOPSIS
+    TODO
+.DESCRIPTION
+    TODO
+.EXAMPLE
+    Write-ActiveDirectoryLogObject -Action SetUser -LogMessage -Result Success -User $user.SamAccountName -LogError $null
+.NOTES
+    Author: jethro@regenit.cloud / https://github.com/regen-it
+    Version: 0.1
+#>
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$action,
+        [Parameter(Mandatory=$true)]
+        [string]$logMessage,
+        [Parameter(Mandatory=$true)]
+        [string]$result,
+        [Parameter(Mandatory=$true)]
+        [AllowEmptyString()]
+        [string]$User,
+        [string]$logError = ($Error | Select-Object -First 1 -ExpandProperty Exception).Message
+    )
+    $logObject = [PSCustomObject]@{
+        Action = $action
+        LogMessage = $logMessage
+        Result = $result
+        User = $user
+        LogError = $logError
+    }
+    $logObject
+}
